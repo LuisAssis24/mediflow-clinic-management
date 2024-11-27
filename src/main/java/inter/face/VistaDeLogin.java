a59439baf99a1fe1525aaaccd51e7282d288ce29
Jao <you@example.com>
2024-11-27 13:21:05 +0000

    Refactoring

--- src/main/java/inter/face/VistaDeLogin.java
+++ src/main/java/inter/face/VistaDeLogin.java
@@ -12,6 +12,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
+
 /**
  *
  * @author rodri
@@ -172,15 +174,17 @@ public class VistaDeLogin extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_nomeUtilizadorActionPerformed
 
+
+
     private void botaoLoginActionPerformed(java.awt.event.ActionEvent evt) {
         // Obtém o nome de utilizador e a palavra-passe inseridos pelo utilizador
         String utilizador = nomeUtilizador.getText();
         String senha = new String(password.getPassword());
 
         // Verifica se as credenciais são válidas
-        if (verificarLogin(utilizador, senha)) {
+        if (SqlServer.verificarLogin(utilizador, senha)) {
             // Determina o tipo de utilizador com base no nome fornecido
-            String tipoUtilizador = verificarTipoUtilizador(utilizador);
+            String tipoUtilizador = SqlServer.verificarTipoUtilizador(utilizador);
 
             if ("Funcionario".equalsIgnoreCase(tipoUtilizador)) {
                 // Caso seja um funcionário, abre a vista correspondente
@@ -205,95 +209,15 @@ public class VistaDeLogin extends javax.swing.JFrame {
             // Se as credenciais forem inválidas, exibe uma mensagem de erro
             JOptionPane.showMessageDialog(this, "Credenciais incorretas. Tente novamente.", "Erro", JOptionPane.ERROR_MESSAGE);
         }
+
     }
 
-    private boolean verificarLogin(String utilizador, String senha) {
 
-        Connection conexao = SqlServer.DatabaseConnection.getInstance();// Obtém a conexão com a base de dados
 
-        if (conexao != null) {  // Verifica se a conexão foi estabelecida com sucesso
-            try {
-                // Declara uma consulta SQL para verificar as credenciais
-                String sql = "SELECT * FROM Utilizador WHERE ID = ? AND Password = ?"; // Vai a tabela utilizador e verifica o Nome e a Password
-                PreparedStatement statement = conexao.prepareStatement(sql);
 
-                // Substitui os placeholders (?) pelos valores fornecidos pelo utilizador
-                statement.setString(1, utilizador);
-                statement.setString(2, senha);
 
-                // Executa a consulta a base de dados e armazena o resultado
-                ResultSet resultado = statement.executeQuery();
 
-                // Retorna verdadeiro se encontrar um registo correspondente
-                return resultado.next();
-            } catch (SQLException e) { // Trata erros relacionados ao SQL
-                System.out.println("Erro ao verificar as credenciais: " + e.getMessage());
-            }
-        } else {
-            // Se a conexão não for estabelecida, exibe uma mensagem de erro
-            System.out.println("Erro de conexão com o banco de dados.");
-        }
-        return false; // Retorna falso se a conexão falhar ou as credenciais forem inválidas
-    }
 
-    private String verificarTipoUtilizador(String utilizador) {
-
-        Connection conexao = SqlServer.DatabaseConnection.getInstance(); // Obtém a conexão com a base de dados
-
-        if (conexao != null) { // Verifica se a conexão foi estabelecida com sucesso
-            try {
-                // Consulta SQL para obter o ID do utilizador com base no nome
-                String sqlId = "SELECT id FROM Utilizador WHERE ID = ?";
-                PreparedStatement stmtId = conexao.prepareStatement(sqlId);
-                stmtId.setString(1, utilizador);
-
-                // Executa a consulta e armazena o resultado
-                ResultSet rsId = stmtId.executeQuery();
-                if (rsId.next()) { // Se encontrar um registo
-                    int userId = rsId.getInt("id"); // Obtém o ID do utilizador
-
-                    // Verifica se o ID corresponde a um funcionário
-                    if (idExisteNaTabela(conexao, userId, "Funcionario", "ID_Funcionario")) {
-                        return "Funcionario";
-                    }
-
-                    // Verifica se o ID corresponde a um gestor
-                    if (idExisteNaTabela(conexao, userId, "Gestor", "ID_Gestor")) {
-                        return "Gestor";
-                    }
-
-                    //Verifica se o ID corresponde a um medico
-                    if (idExisteNaTabela(conexao, userId, "Medico", "ID_Medico")){
-                    return "Medico";
-                    }
-
-                    // Adicionar mais verificações aqui para outros tipos de utilizadores, se necessário
-                }
-            } catch (SQLException e) { // Trata erros relacionados ao SQL
-                System.out.println("Erro ao verificar o tipo de usuário: " + e.getMessage());
-            }
-        }
-
-        return null; // Retorna null se não encontrar o tipo do utilizador
-    }
-
-    private boolean idExisteNaTabela(Connection conexao, int id, String tabela, String colunaId) {
-        try {
-            // Declara uma consulta SQL para verificar a existência do ID na tabela
-            String sql = "SELECT " + colunaId + " FROM " + tabela + " WHERE " + colunaId + " = ?";
-            PreparedStatement stmt = conexao.prepareStatement(sql);
-
-            // Substitui o placeholder (?) pelo valor do ID
-            stmt.setInt(1, id);
-
-            // Executa a consulta e verifica se existe pelo menos um registo correspondente
-            ResultSet rs = stmt.executeQuery();
-            return rs.next(); // Retorna verdadeiro se encontrar um registo
-        } catch (SQLException e) { // Trata erros relacionados ao SQL
-            System.out.println("Erro ao verificar a tabela " + tabela + ": " + e.getMessage());
-        }
-        return false; // Retorna falso se ocorrer um erro ou não encontrar o ID
-    }
 
     private void passwordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordActionPerformed
         // TODO add your handling code here:
--- src/main/java/sql/server/SqlServer.java
+++ src/main/java/sql/server/SqlServer.java
@@ -1,10 +1,99 @@
 package sql.server;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SqlServer {
+
+    public static boolean verificarLogin(String utilizador, String senha) {
+
+        Connection conexao = SqlServer.DatabaseConnection.getInstance();// Obtém a conexão com a base de dados
+
+        if (conexao != null) {  // Verifica se a conexão foi estabelecida com sucesso
+            try {
+                // Declara uma consulta SQL para verificar as credenciais
+                String sql = "SELECT * FROM Utilizador WHERE ID = ? AND Password = ?"; // Vai a tabela utilizador e verifica o Nome e a Password
+                PreparedStatement statement = conexao.prepareStatement(sql);
+
+                // Substitui os placeholders (?) pelos valores fornecidos pelo utilizador
+                statement.setString(1, utilizador);
+                statement.setString(2, senha);
+
+                // Executa a consulta a base de dados e armazena o resultado
+                ResultSet resultado = statement.executeQuery();
+
+                // Retorna verdadeiro se encontrar um registo correspondente
+                return resultado.next();
+            } catch (SQLException e) { // Trata erros relacionados ao SQL
+                System.out.println("Erro ao verificar as credenciais: " + e.getMessage());
+            }
+        } else {
+            // Se a conexão não for estabelecida, exibe uma mensagem de erro
+            System.out.println("Erro de conexão com o banco de dados.");
+        }
+        return false; // Retorna falso se a conexão falhar ou as credenciais forem inválidas
+    }
+
+
+
+
+    public static String verificarTipoUtilizador(String utilizador) {
+
+        Connection conexao = SqlServer.DatabaseConnection.getInstance(); // Obtém a conexão com a base de dados
+
+        if (conexao != null) { // Verifica se a conexão foi estabelecida com sucesso
+            try {
+                // Consulta SQL para obter o ID do utilizador com base no nome
+                String sqlId = "SELECT id FROM Utilizador WHERE ID = ?";
+                PreparedStatement stmtId = conexao.prepareStatement(sqlId);
+                stmtId.setString(1, utilizador);
+
+                // Executa a consulta e armazena o resultado
+                ResultSet rsId = stmtId.executeQuery();
+                if (rsId.next()) { // Se encontrar um registo
+                    int userId = rsId.getInt("id"); // Obtém o ID do utilizador
+
+                    // Verifica se o ID corresponde a um funcionário
+                    if (idExisteNaTabela(conexao, userId, "Funcionario", "ID_Funcionario")) {
+                        return "Funcionario";
+                    }
+
+                    // Verifica se o ID corresponde a um gestor
+                    if (idExisteNaTabela(conexao, userId, "Gestor", "ID_Gestor")) {
+                        return "Gestor";
+                    }
+
+                    //Verifica se o ID corresponde a um medico
+                    if (idExisteNaTabela(conexao, userId, "Medico", "ID_Medico")){
+                        return "Medico";
+                    }
+
+                    // Adicionar mais verificações aqui para outros tipos de utilizadores, se necessário
+                }
+            } catch (SQLException e) { // Trata erros relacionados ao SQL
+                System.out.println("Erro ao verificar o tipo de usuário: " + e.getMessage());
+            }
+        }
+
+        return null; // Retorna null se não encontrar o tipo do utilizador
+    }
+
+    private static boolean idExisteNaTabela(Connection conexao, int id, String tabela, String colunaId) {
+        try {
+            // Declara uma consulta SQL para verificar a existência do ID na tabela
+            String sql = "SELECT " + colunaId + " FROM " + tabela + " WHERE " + colunaId + " = ?";
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+
+            // Substitui o placeholder (?) pelo valor do ID
+            stmt.setInt(1, id);
+
+            // Executa a consulta e verifica se existe pelo menos um registo correspondente
+            ResultSet rs = stmt.executeQuery();
+            return rs.next(); // Retorna verdadeiro se encontrar um registo
+        } catch (SQLException e) { // Trata erros relacionados ao SQL
+            System.out.println("Erro ao verificar a tabela " + tabela + ": " + e.getMessage());
+        }
+        return false; // Retorna falso se ocorrer um erro ou não encontrar o ID
+    }
 
     // A classe DatabaseConnection é a responsavel por criar a conexão com a base de dados
     public class DatabaseConnection {
@@ -36,6 +125,9 @@ public class SqlServer {
             // Retorna a instância da conexão, que pode ser null se não for criada
             return connection;
         }
+
+
+
 
         // Método estático para fechar a conexão com a base de dados
         public static void closeConnection() {
